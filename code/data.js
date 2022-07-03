a1d1143fa89cacb6afc2841abd9d022d8ee44c0b
daridev <hernandezdarifrancisco@gmail.com>
2022-07-02 22:49:37 -0500

    get data from github api

--- code/data.js
+++ code/data.js
@@ -55,45 +55,45 @@ const best_posts = [
 ]
 
 
-const instagram = {
-    profile_photo: "https://www.darideveloper.com/imgs/logo.png",
-    profile_name: "darideveloper",
-    name: "francisco dari hernandez",
-    num_post: 8,
-    num_followers: 23,
-    num_following: 31,
-    category: "Software",
-    details: "Desarrollador frontend, desarrollador wordpress y experto en web scraping.",
-    web_page_text: "www.darideveloper.com",
-    web_page_link: "www.darideveloper.com",
-    profile_url: "https://www.instagram.com/darideveloper/",
-    posts: [
-        {
-            image: "https://i.imgur.com/hCla2Dz.png",
-            url: "https://www.instagram.com/p/CfUkBDnpKuj/"
-        },
-        {
-            image: "https://i.imgur.com/hCla2Dz.png",
-            url: "https://www.instagram.com/p/CfUkBDnpKuj/"
-        },
-        {
-            image: "https://i.imgur.com/hCla2Dz.png",
-            url: "https://www.instagram.com/p/CfUkBDnpKuj/"
-        },
-        {
-            image: "https://i.imgur.com/hCla2Dz.png",
-            url: "https://www.instagram.com/p/CfUkBDnpKuj/"
-        },
-        {
-            image: "https://i.imgur.com/hCla2Dz.png",
-            url: "https://www.instagram.com/p/CfUkBDnpKuj/"
-        },
-        {
-            image: "https://i.imgur.com/hCla2Dz.png",
-            url: "https://www.instagram.com/p/CfUkBDnpKuj/"
-        },
-    ],
-}
+// const instagram = {
+//     profile_photo: "https://instagram.fagu3-1.fna.fbcdn.net/v/t51.2885-19/272281480_643776326936289_5502227003927626832_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fagu3-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=B8RZp8B6kVEAX_vOdLs&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT_soWsVWki84axJSzOR-rfsMpkSFFmw9tmtPmHm5t51SA&oe=62C7A15D&_nc_sid=8fd12b",
+//     profile_name: "darideveloper",
+//     name: "francisco dari hernandez",
+//     num_post: 8,
+//     num_followers: 23,
+//     num_following: 31,
+//     category: "Software",
+//     details: "Desarrollador frontend, desarrollador wordpress y experto en web scraping.",
+//     web_page_text: "www.darideveloper.com",
+//     web_page_link: "www.darideveloper.com",
+//     profile_url: "https://www.instagram.com/darideveloper/",
+//     posts: [
+//         {
+//             image: "https://i.imgur.com/hCla2Dz.png",
+//             url: "https://www.instagram.com/p/CfUkBDnpKuj/"
+//         },
+//         {
+//             image: "https://i.imgur.com/hCla2Dz.png",
+//             url: "https://www.instagram.com/p/CfUkBDnpKuj/"
+//         },
+//         {
+//             image: "https://i.imgur.com/hCla2Dz.png",
+//             url: "https://www.instagram.com/p/CfUkBDnpKuj/"
+//         },
+//         {
+//             image: "https://i.imgur.com/hCla2Dz.png",
+//             url: "https://www.instagram.com/p/CfUkBDnpKuj/"
+//         },
+//         {
+//             image: "https://i.imgur.com/hCla2Dz.png",
+//             url: "https://www.instagram.com/p/CfUkBDnpKuj/"
+//         },
+//         {
+//             image: "https://i.imgur.com/hCla2Dz.png",
+//             url: "https://www.instagram.com/p/CfUkBDnpKuj/"
+//         },
+//     ],
+// }
 
 
 function sleep(s) {
@@ -109,8 +109,9 @@ export async function get_posts () {
 }
 
 export async function get_instagram () {
-    sleep (1)
-    return instagram
+    return fetch ("https://raw.githubusercontent.com/DariHernandez/instagram-last-posts-github-api/master/data.json")
+    .then (reponse => reponse.json())
+    .then (json_data => json_data["darideveloper"])
 }
 
 
